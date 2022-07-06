e8dba49fc368eb0b829efbae73056912670fa619
gwhitney15 <gwhitney115@gmail.com>
2022-07-05 23:40:22 -0500

    first commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Attempted to make the password generator. This project is uncomplete. Due to the holiday weekend, I was unable to find time to recieve tutoring on this. I intend to recieve tutoring on this assignment to further alter it and learn from what I have done wrong this week.
+
+This project is used to collect information on certain critera when the generate password button is clicked and output a randomly generated password.
+
+Added prompts to collect how long the password should be as well as used confirms to include certain password criteria.
+
+When prompts are selected, the console logs a string of all selected criteria. I attempted and failed to add a functioning way to randomize a password of appropriate length from this generated string. I was unsure of how to make this work and intend to recieve tutoring on this part of the assignment. 
+
---
+++ script.js
@@ -0,0 +1,59 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var characters = "abcdefghijklmnopqrstuvwxyz";
+var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var charSpecial = "!@,#$%&*{}[]/\\+=";
+
+var allAnswers
+var result = '';
+
+
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+
+
+
+    var passLength = prompt("How many characters would you like your password to be? (8-128)")
+    if (passLength < 8 || passLength > 128) {
+        alert("Please enter a valid number.");
+        writePassword()
+    } else {
+        var passLower = confirm("Would you like to include lowercase characters?");
+        var passUpper = confirm("Would you like to include uppercase characters?");
+        var passNumber = confirm("Would you like to include numbers?");
+        var passSpecial = confirm("Would you like to include special characters?");
+
+
+
+        if (!passLower && !passUpper && !passNumber && !passSpecial) {
+            alert("Plese select at least one option.")
+            writePassword()
+        }
+        if (passLower) { allAnswers += characters }
+        if (passUpper) { allAnswers += charactersUpper }
+        if (passNumber) { allAnswers += numbers }
+        if (passSpecial) { allAnswers += charSpecial }
+
+    }
+    console.log(allAnswers)
+    return allAnswers
+
+}
+
+
+
+
+
+
